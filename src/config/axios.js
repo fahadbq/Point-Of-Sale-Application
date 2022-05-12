e578805f9161d951c272a74783d351adeb9234b2
fahd <daylight1498@gmail.com>
2022-05-12 18:37:35 +0530

    changed confiq token header

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,9 +2,9 @@ import Axios from 'axios'
 
 const axios = Axios.create({
     baseURL: `http://dct-pos-app.herokuapp.com/api`,
-        headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        }
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
 })
 
 export default axios
