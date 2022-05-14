d423e0128eae2fbc80cee612172ba99cfa21ba6d
Fahad <daylight1498@gmail.com>
2022-05-15 03:50:05 +0530

    added hi text in dashboard

--- src/components/dashboard/DashboardContainer.js.js
+++ src/components/dashboard/DashboardContainer.js.js
@@ -10,12 +10,12 @@ const DashboardContainer = (props) => {
 
     return (
         <div>
-            <div style={{ position: "relative", right: "90px", top: "80px", color: "#404040" }} >
+            <div style={{ position: "absolute", right: "90px", top: "90px", color: "#8E24AA" }} >
                 {user.loading ? (
                     <div className="spinner-border text-success" role="status" >
                     <h2 className="visually-hidden" > loading User </h2>
                     </div>
-                    ) : (<h2 > Hi, {user.data.username} </h2>)} <br />
+                    ) : (<h2> Hi, {user.data.username} </h2>)} <br />
             </div>
 
             <DashboardChart />
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: `http://dct-pos-app.herokuapp.com/api`,
+    baseURL: `https://dct-pos-app.herokuapp.com/api`,
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
