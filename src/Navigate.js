c38d7c7d67ec8445b028cf1d6a72f2290747f454
Fahad <daylight1498@gmail.com>
2022-06-05 23:46:03 +0530

    makeitbetter

--- src/Navigate.js
+++ src/Navigate.js
@@ -1,95 +1,139 @@
-import { Routes, Route, NavLink } from 'react-router-dom'
-import PrivateRoute from './helperComponents/PrivateRoute'
-import swal from 'sweetalert'
+import { Routes, Route, NavLink, useNavigate } from "react-router-dom";
+import PrivateRoute from "./helperComponents/PrivateRoute";
+import swal from "sweetalert";
 
-import Home from './userAuthComponents/Home'
-import Register from './userAuthComponents/Register'
-import Login from './userAuthComponents/Login'
-import Account from './components/Account'
-import DashboardContainer from './components/dashboard/DashboardContainer.js'
-import CustomersContainer from './components/customers/CustomersContainer'
-import ProductsContainer from './components/products/ProductsContainer'
-import BillsContainer from './components/bills/BillsContainer'
-import BillDetails from './components/bills/BillDetails'
+import Home from "./userAuthComponents/Home";
+import Register from "./userAuthComponents/Register";
+import Login from "./userAuthComponents/Login";
+import Account from "./components/Account";
+import DashboardContainer from "./components/dashboard/DashboardContainer.js";
+import CustomersContainer from "./components/customers/CustomersContainer";
+import ProductsContainer from "./components/products/ProductsContainer";
+import BillsContainer from "./components/bills/BillsContainer";
+import BillDetails from "./components/bills/BillDetails";
 
+const Navigate = (props) => {
+  const { userLoggedIn, handleAuth } = props;
+  const navigate = useNavigate();
 
-const Navigate = (props) =>{
-    const { userLoggedIn, handleAuth } = props
+  const handleLogOut = () => {
+    swal({
+      title: "Are you sure?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        localStorage.removeItem("token");
+        handleAuth();
+        swal("Logged out", {
+          icon: "success",
+        });
+        navigate("/login");
+      }
+    });
+  };
 
+  return (
+    <div>
+      {userLoggedIn ? (
+        <nav
+          className="navbar sticky-top navbar-expand-lg navbar-dark"
+          style={{ backgroundColor: "#263238" }}
+        >
+          <div className="container-fluid">
+            <NavLink to="/dashboard" className="navbar-brand active ">
+              {" "}
+              Dashboard{" "}
+            </NavLink>
 
-    const handleLogOut = () =>{
-        swal({
-            title: "Are you sure?",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-                localStorage.removeItem('token')
-                handleAuth()
-              swal("Logged out", {
-                icon: "success",
-              });
-              props.history.push('/login')
-            }
-          });
-    }
+            <div className="collapse navbar-collapse">
+              <NavLink to="/customers" className="navbar-brand nav-link active">
+                {" "}
+                Customers{" "}
+              </NavLink>
 
+              <NavLink to="/products" className="navbar-brand nav-link active">
+                {" "}
+                Products{" "}
+              </NavLink>
 
-    return (
-        <div >
+              <NavLink to="/bills" className="navbar-brand nav-link active">
+                {" "}
+                Bills{" "}
+              </NavLink>
+            </div>
 
-            { userLoggedIn ? (
-                    
-                <nav className="navbar sticky-top navbar-expand-lg navbar-dark" style={{backgroundColor: "#263238"}}>
-                
-                    <div className="container-fluid" >
-                        <NavLink to='/dashboard' className="navbar-brand active " > Dashboard </NavLink>
+            <NavLink to="/account" className="navbar-brand nav-link active">
+              {" "}
+              Account{" "}
+            </NavLink>
 
-                        <div className="collapse navbar-collapse" >
-                            <NavLink to='/customers' className="navbar-brand nav-link active" > Customers </NavLink>
+            <NavLink
+              to="/account"
+              onClick={handleLogOut}
+              className="navbar-brand nav-link active"
+            >
+              {" "}
+              Log Out{" "}
+            </NavLink>
+          </div>
+        </nav>
+      ) : (
+        <nav
+          className="navbar sticky-top navbar-expand-lg navbar-dark"
+          style={{ backgroundColor: "#263238" }}
+        >
+          <div className="container-fluid">
+            <div className="collapse navbar-collapse">
+              <NavLink to="/" className="navbar-brand nav-link active">
+                {" "}
+                Home{" "}
+              </NavLink>
+              <NavLink to="/register" className="navbar-brand nav-link active">
+                {" "}
+                Register{" "}
+              </NavLink>
+              <NavLink to="/login" className="navbar-brand nav-link active">
+                {" "}
+                Login{" "}
+              </NavLink>
+            </div>
+          </div>
+        </nav>
+      )}
 
-                            <NavLink to='/products' className="navbar-brand nav-link active" > Products </NavLink>
-                            
-                            <NavLink to='/bills' className="navbar-brand nav-link active" > Bills </NavLink>
-                        </div>
-                        
-                        <NavLink to='/account' className="navbar-brand nav-link active" > Account </NavLink>
+      {/* Route session */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login handleAuth={handleAuth} />} />
 
-                        <NavLink to='/account' onClick={ handleLogOut } className="navbar-brand nav-link active" > Log Out </NavLink>
-                    </div>
-                </nav>
-            ) : (
-                <nav className="navbar sticky-top navbar-expand-lg navbar-dark" style={{backgroundColor: "#263238"}}>
+        {/* Private route */}
+        <Route path="/account" element={<PrivateRoute component={Account} />} />
+        <Route
+          path="/customers"
+          element={<PrivateRoute component={CustomersContainer} />}
+        />
+        <Route
+          path="/products"
+          element={<PrivateRoute component={ProductsContainer} />}
+        />
+        <Route
+          path="/bills"
+          element={<PrivateRoute component={BillsContainer} />}
+        />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute component={DashboardContainer} />}
+        />
+        <Route
+          path="/bills/:id"
+          element={<PrivateRoute component={BillDetails} />}
+        />
+      </Routes>
+    </div>
+  );
+};
 
-                    <div className="container-fluid" >
-                        <div className="collapse navbar-collapse" >
-                            <NavLink to='/' className="navbar-brand nav-link active" > Home </NavLink> 
-                            <NavLink to='/register' className="navbar-brand nav-link active" > Register </NavLink> 
-                            <NavLink to='/login' className="navbar-brand nav-link active" > Login </NavLink> 
-                        </div>
-                    </div>
-                </nav>
-            )}
-
-
-            {/* Route session */}
-            <Routes >
-                <Route  path='/' element={<Home />} />
-                <Route  path='/register' element={<Register />} />
-                <Route  path='/login' element={<Login handleAuth={handleAuth} />} />
-
-                {/* Private route */}
-                <Route path='/account' element={<PrivateRoute component={Account} />}  />
-                <Route path='/customers' element={<PrivateRoute component={CustomersContainer} />}  />
-                <Route path='/products' element={<PrivateRoute component={ProductsContainer} />}  />
-                <Route path='/bills' element={<PrivateRoute component={BillsContainer} />}  />
-                <Route path='/dashboard' element={<PrivateRoute component={DashboardContainer} />}  />
-                <Route path='/bills/:id' element={<PrivateRoute component={BillDetails} />} />
-            </Routes>
-        </div>
-    )
-}
-
-export default Navigate
+export default Navigate;
--- src/components/bills/BillForm.js
+++ src/components/bills/BillForm.js
@@ -1,148 +1,195 @@
-import React, { useState } from 'react'
-import { Formik, Form, Field } from 'formik'
-import * as yup from 'yup'
-import { useSelector } from "react-redux"
-import { useDispatch } from 'react-redux'
-import { asyncAddBills } from '../../redux/actions/billsAction'
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as yup from "yup";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { asyncAddBills } from "../../redux/actions/billsAction";
 
 import { FaTrashAlt } from "react-icons/fa";
 
 const BillForm = (props) => {
-    //Adding lineItems to input fields 
-    const [ inputFields, setInputFields ] = useState([{ product: '', quantity: 1 }])
-    
-    const dispatch = useDispatch()
+  //Adding lineItems to input fields
+  const [inputFields, setInputFields] = useState([
+    { product: "", quantity: 1 },
+  ]);
 
-    const { customers, products } = useSelector( state => state) //Reading state variables from Redux
+  const dispatch = useDispatch();
 
-    //Reading input values dynamically
-    const handleDynamicChange = (index, e) => {
-        const values = [ ...inputFields]
-        values[index][e.target.name] = e.target.value
-        setInputFields(values)
-    }
+  const { customers, products } = useSelector((state) => state); //Reading state variables from Redux
 
-    const handleAddField = () => {
-        setInputFields([ ...inputFields, { product: '', quantity: 1 }])
-    }
+  //Reading input values dynamically
+  const handleDynamicChange = (index, e) => {
+    const values = [...inputFields];
+    values[index][e.target.name] = e.target.value;
+    setInputFields(values);
+  };
 
-    const handleRemoveField = (index) => { 
-        const values = [ ...inputFields]
-        values.splice(index, 1)
-        setInputFields(values)
-    }
+  const handleAddField = () => {
+    setInputFields([...inputFields, { product: "", quantity: 1 }]);
+  };
 
-    const resetField = () => {
+  const handleRemoveField = (index) => {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  };
 
-        setInputFields([ { product: '', quantity: 0 } ])
-    }
+  const resetField = () => {
+    setInputFields([{ product: "", quantity: 0 }]);
+  };
 
-    //Formik
-    const initialValues = {
-        date: "",
-        customer: "",
-    }
+  //Formik
+  const initialValues = {
+    date: "",
+    customer: "",
+  };
 
-    const validationSchema = yup.object({
-        date: yup.string().required('Date is required!'),
-        customer: yup.string().required('Select a customer!')
-    })
+  const validationSchema = yup.object({
+    date: yup.string().required("Date is required!"),
+    customer: yup.string().required("Select a customer!"),
+  });
 
-    return (
-        <div className='border shadow-sm p-3 mb-5 bg-body rounded' style={{ position: "absolute", top: '105px', right: "50px", width: "500px", height: "480px" }} >
-            
-            <h4> Create a Bill </h4> <hr />
+  return (
+    <div
+      className="border shadow-sm p-3 mb-5 bg-body rounded"
+      style={{
+        position: "absolute",
+        top: "105px",
+        right: "50px",
+        width: "500px",
+        height: "480px",
+      }}
+    >
+      <h4> Create a Bill </h4> <hr />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values, { resetForm }) => {
+          values.lineItems = [...inputFields]; // Creating new property before Submitting a form.
+          dispatch(asyncAddBills(values, resetField, resetForm));
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className="row g-3 needs-validation" noValidate>
+            <div className="col-md-6">
+              <Field type="date" name="date" className="form-control" />
 
-            <Formik 
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={(values, { resetForm }) => {
-                    values.lineItems = [ ...inputFields] // Creating new property before Submitting a form.
-                    dispatch(asyncAddBills(values, resetField, resetForm))
-                }}
-            >   
-            
-                { ({ errors, touched }) => (
-                    <Form className="row g-3 needs-validation" noValidate >
+              {errors.date && touched.date ? (
+                <div>
+                  {" "}
+                  <span className="form-text" style={{ color: "red" }}>
+                    {" "}
+                    {errors.date}{" "}
+                  </span>{" "}
+                </div>
+              ) : null}
+            </div>
 
+            <div className="col-md-6">
+              <Field as="select" name="customer" className="form-select">
+                <option value=""> Select Customer </option>
+
+                {customers.data.map((cust) => {
+                  return (
+                    <option key={cust._id} value={cust._id}>
+                      {" "}
+                      {cust.name}{" "}
+                    </option>
+                  );
+                })}
+              </Field>
+              {errors.customer && touched.customer ? (
+                <div>
+                  {" "}
+                  <span className="form-text" style={{ color: "red" }}>
+                    {" "}
+                    {errors.customer}{" "}
+                  </span>{" "}
+                </div>
+              ) : null}
+            </div>
+
+            <div
+              style={{
+                height: "18rem",
+                overflowX: "scroll",
+                display: "block",
+              }}
+            >
+              {inputFields.map((field, index) => {
+                return (
+                  <div key={index} className="row g-3">
                     <div className="col-md-6">
-                        <Field type="date" 
-                            name="date"
-                            className="form-control"
-                        />
+                      <select
+                        value={field.product}
+                        name="product"
+                        onChange={(e) => handleDynamicChange(index, e)}
+                        className="form-select"
+                      >
+                        <option value=""> Select Product </option>
 
-                        { errors.date && touched.date ? <div> <span className="form-text" style={{color: "red"}} > { errors.date } </span> </div> : null }
+                        {products.data.map((prod) => {
+                          return (
+                            <option key={prod._id} value={prod._id}>
+                              {" "}
+                              {prod.name}{" "}
+                            </option>
+                          );
+                        })}
+                      </select>
                     </div>
 
-                    <div className="col-md-6" >
-                        <Field as="select" name="customer" className="form-select" >
-                            <option value=''> Select Customer </option>
-
-                            { customers.data.map((cust) =>{
-                                return <option key={ cust._id } value={cust._id} > {cust.name} </option>
-                            })}
-                        </Field>
-                        { errors.customer && touched.customer ? <div> <span className="form-text" style={{color: 'red'}} > { errors.customer } </span> </div> : null }
+                    <div className="col-md-2">
+                      <input
+                        type="number"
+                        value={field.quantity}
+                        name="quantity"
+                        onChange={(e) => handleDynamicChange(index, e)}
+                        min="1"
+                        max="99"
+                        className="form-control"
+                        style={{ width: "70px" }}
+                      />
                     </div>
-                    
-                    <div style={{
-                     height: "18rem",
-                     overflow: "scroll",
-                     display: "block"
-                    }} 
-                    >
-                        { inputFields.map(( field, index) =>{
-                        return <div key={index} className="row g-3" >
 
-                            <div className="col-md-6" >
-                                <select value={field.product} name='product' onChange={(e) => handleDynamicChange(index, e) } className="form-select" > 
-                                    <option value=''> Select Product </option>
-
-                                    { products.data.map((prod) =>{
-                                        return <option key={ prod._id } value={prod._id} > {prod.name} </option>
-                                    })}
-                                </select>
-                            </div>
-                            
-                            <div className="col-md-2" >
-                                <input type='number' 
-                                    value={field.quantity} 
-                                    name='quantity' 
-                                    onChange={(e) => handleDynamicChange(index, e)} 
-                                    min='1' 
-                                    max='99' 
-                                    className='form-control'
-                                    style={{width: "70px"}} 
-                                    
-                                />
-                            </div>
-
-                            <div className="col-md-2" >
-                                <input type='button' value='Add' onClick={() => {
-                                    handleAddField()
-                                }} className="btn btn-success" />
-                            </div>
-
-                            <div className="col-md-2" >
-                                { index ? 
-                                <button type='button' 
-                                    onClick={() => handleRemoveField(index) } 
-                                    className="btn btn-danger" > <FaTrashAlt /> 
-                                </button> : null }
-                            </div>
-
-                        </div>
-                    }) } <br />
-
+                    <div className="col-md-2">
+                      <input
+                        type="button"
+                        value="Add"
+                        onClick={() => {
+                          handleAddField();
+                        }}
+                        className="btn btn-success"
+                      />
                     </div>
-                    
-                    <button type='submit' className="btn btn-primary btn-sm d-grid" > Submit </button>
-                </Form>
-                )}
 
-            </Formik>
-        </div>
-    )
-}
+                    <div className="col-md-2">
+                      {index ? (
+                        <button
+                          type="button"
+                          onClick={() => handleRemoveField(index)}
+                          className="btn btn-danger"
+                        >
+                          {" "}
+                          <FaTrashAlt />
+                        </button>
+                      ) : null}
+                    </div>
+                  </div>
+                );
+              })}{" "}
+              <br />
+            </div>
 
-export default BillForm
+            <button type="submit" className="btn btn-primary btn-sm d-grid">
+              {" "}
+              Submit{" "}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default BillForm;
